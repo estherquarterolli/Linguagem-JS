4283fa528a83c28cfb0255c5f37b8b03ea340c77
Esther Quarterolli <115496105+estherquarterolli@users.noreply.github.com>
2025-09-07 16:49:25 -0300

    att

---
+++ condicoespt1.js
@@ -0,0 +1,6 @@
+console.log('Funcionou corretamente');
+
+var vel= 50;
+
+console.log(`Dirija com cuidado! A velocidade máxima é ${vel} km/h`)
+console.log('Funcionou corretamente');
---
+++ ifeelse.js
@@ -0,0 +1,7 @@
+var pais= 'Brasil';
+if(pais == 'Brasil'){
+    console.log(`Você mora no país ${pais}, então é brasileiro`);
+
+}else{
+    console.log(`Você é gringo! Pois seu país é ${pais}`)
+}
